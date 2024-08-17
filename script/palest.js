0308bacb2cd488ab2028b15921d1ef2591d2ed59
Eduardo Perottoni <edu.perottoni@gmail.com>
2024-08-17 02:27:08 -0300

    New style to carousel

--- script/palest.js
+++ script/palest.js
@@ -9,7 +9,7 @@ $('.carrossel').slick({
           arrows: false,
           centerMode: true,
           centerPadding: '40px',
-          slidesToShow: 3
+          slidesToShow: 2
         }
       },
       {
@@ -23,3 +23,46 @@ $('.carrossel').slick({
       }
     ]
   });
+
+
+const peopleInfo = {
+  "John Doe": {
+    name: "John Doe",
+    abstract: "A short description about John Doe.",
+    events: [
+      { title: "Palestra1", date: "22/09", start: "12:00", end: "14:00"},
+      { title: "Palestra2", date: "22/09", start: "12:00", end: "14:00"}
+    ]
+  }
+};
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const palestModal = document.getElementById('palestModal');
+  
+  palestModal.addEventListener('show.bs.modal', function (event) {
+    const button = event.relatedTarget;
+    const personName = button.getAttribute('data-person');
+    const personPhoto = button.getAttribute('data-img-src');
+    
+    const personInfo = peopleInfo[personName];
+
+    
+    if (personInfo) {
+      palestModal.querySelector("#modal-palest-photo").src = `${personPhoto}`
+      // Update the modal content
+      palestModal.querySelector('.modal-title').innerHTML = `${personInfo.name}`;
+      palestModal.querySelector('#palest-abstract').innerHTML = `${personInfo.abstract}`;
+
+      // Populate the list with JSON objects
+      const palestElement = palestModal.querySelector('#palest-events');
+      palestElement.innerHTML = '';  // Clear previous content
+      personInfo.events.forEach(event => {
+        const li = document.createElement('li');
+        li.innerHTML = `<b>${event.title}</b> - ${event.date} (${event.start} até ${event.end})`;
+        palestElement.appendChild(li);
+      });
+    }
+  });
+});
