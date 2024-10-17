1a28a062a41df474a8b64d6c71b03b8d77b47de1
Eduardo Perottoni <edu.perottoni@gmail.com>
2024-10-17 01:10:54 -0300

    Fixing styles according to otavio's PR (solving conflicts by hand didn't work)

---
+++ script/speakers.js
@@ -0,0 +1,118 @@
+/**
+ * Speakers data.
+ */
+
+const speakers = {
+  Ale: {
+    photo: "../img/placeholder-icon.webp",
+    title: "Palestrante",
+
+    abstract: "Estou na quarta fase de sistemas e gosto muito de Python!",
+    events: [
+      {
+        title:
+          "Geocodificação em Python: Uma análise para os números de casas desocupadas em Florianópolis",
+      },
+    ],
+    contact: {
+      email: "ale.benedetp@gmail.com",
+    },
+  },
+  "Cainã Rinaldi Esteche": {
+    photo: "../img/placeholder-icon.webp",
+    title: "Palestrante",
+
+    abstract:
+      "Estudante de Ciência da Computação, Medalhista de ouro da OBMEP e CTO da Mimolda. Atleta de vôlei e xadrez e entusiasta por problemas de matemática e computação.",
+    events: [
+      {
+        title:
+          "Dedos, Palitos e Grafos: Vencendo com Matemática – Um Guia para Dominar Jogos de Estratégia",
+      },
+    ],
+    contact: {
+      email: "cairinest@hotmail.com",
+    },
+  },
+  Rafael: {
+    photo: "../img/placeholder-icon.webp",
+    title: "Palestrante",
+
+    abstract:
+      "Rafael Oliveira é fundador e Head-Engineer do Osiris, um grupo de pesquisa focado no projeto e desenvolvimento de microarquiteturas RISC-V. Possui experiência em projetos  e caracterização elétrica de circuitos integrados em tecnologias nanométricas. Suas principais áreas de expertise são Projeto de Circuitos Digitais e Circuitos Combinacionais Tolerantes a Falhas. Rafael publicou artigos sobre esses temas em importantes periódicos e conferências. Além disso, atua como Projetista de CI Digital na Chipus Microelectronics e é voluntário no Laboratório de Computação Embarcada (ECL-UFSC).",
+    events: [
+      {
+        title:
+          "Entre Bits e Clocks: Desvendando o Projetos de Circuitos Integrados",
+      },
+    ],
+    contact: {
+      email: "rafaelnevesmello@gmail.com",
+    },
+  },
+  "Mario Araujo Xavier": {
+    photo: "../img/placeholder-icon.webp",
+    title: "Palestrante",
+
+    abstract:
+      "Natalense em Florianópolis. Trabalha com infraestrutura, datacenter, virtualização, servidores e nuvem Microsoft Azure, Hyper-V, Windows Server. Já palestrou na Universidade Federal de Santa Catarina (UFSC), Universidade Tecnológica Federal do Paraná (UTFPR), Universidade Federal do Ceará (UFCE), Universidade Federal do Rio Grande do Norte (UFRN) e na Universidade Tiradentes (UNIT) - Recife/PE",
+    events: [
+      {
+        title:
+          "KDE Neon & KDE Plasma: venha para o KDE e ganhe experiencia profissional em TI",
+      },
+      { title: "Oficina de Python para iniciantes" },
+      { title: "Linkedin Avançado" },
+      {
+        title:
+          "FOSSHUB e o desafio de implantar ferramentas livres no mercado corporativo",
+      },
+      { title: "Dokuwiki e a superimportancia da documentacao na TI" },
+    ],
+    contact: {
+      email: "marioaxavier7@gmail.com",
+    },
+  },
+  "Matheus de Oliveira Saldanha": {
+    photo: "../img/placeholder-icon.webp",
+    title: "Palestrante",
+
+    abstract:
+      "Bacharel em Ciências Computação pela UFSC, fazendo parte do LabSEC durante a graduação. Atualmente, mestrando em Ciências da Computação pela UFSC, focando os estudos em Isogeny-based Cryptography e trabalhando como engenheiro de software na Motorola.",
+    events: [
+      { title: "Criptografia da Troca de Chaves: Clássica à Pós-Quântica" },
+    ],
+    contact: {
+      email: "zinhoo02@gmail.com",
+    },
+  },
+  "Samuel Cardoso": {
+    photo: "../img/placeholder-icon.webp",
+    title: "Palestrante",
+
+    abstract:
+      "Me chamo Samuel Cardoso, sou formado em Ciência da Computação pela UFSC e atualmente atuo como Analista de Segurança da Informação na Conviso Appsec, realizando Pentests e outras operações de segurança.",
+    events: [
+      {
+        title:
+          "Como entender vulnerabilidades: Do compilador a ReDoS + Live Hacking",
+      },
+    ],
+    contact: {
+      email: "samuelcardosodejesus@hotmail.com",
+    },
+  },
+  "Carla Merkle Westphall": {
+    photo: "../img/placeholder-icon.webp",
+    title: "Professora",
+
+    abstract:
+      "Carla Merkle Westphall é graduada em Ciências da Computação pela UFSC em 1994, obteve o grau de mestre em Ciência da Computação pela UFSC em 1996 e o grau de Doutora em Engenharia Elétrica na área de Segurança em 2000 pela UFSC. Atua na área de Ciência da Computação desde 1987. Participou de vários projetos de pesquisa na área de Segurança de Sistemas. O projeto atual chama-se Segurança nas Redes da Nova Geração, englobando Internet das coisas, computação em nuvem, computação em névoa e redes sem fio. Este projeto está em desenvolvimento no Laboratório de Redes e Gerência (LRG) da UFSC, com a participação de alunos de doutorado e mestrado no Programa de Pós-Graduação em Ciência da Computação (PPGCC) da UFSC, além de alunos de graduação. Tem interesse pelos seguintes temas de pesquisa: segurança em sistemas distribuídos, redes de nova geração, blockchain, segurança na programação e gerenciamento de identidades.",
+    events: [{ title: "Mesa Redonda" }],
+    contact: {
+      email: "carla.merkle.westphall@ufsc.br",
+    },
+  },
+};
+
+export { speakers };
