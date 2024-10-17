ad3b8717b6c9e89b42ef4954d72c288ddb75127e
Eduardo Perottoni <edu.perottoni@gmail.com>
2024-10-17 02:35:35 -0300

    Adding speakers data

--- script/speakers.js
+++ script/speakers.js
@@ -3,7 +3,7 @@
  */
 
 const speakers = {
-  Ale: {
+  "Ale": {
     photo: "../img/placeholder-icon.webp",
     title: "Palestrante",
 
@@ -19,7 +19,7 @@ const speakers = {
     },
   },
   "Cainã Rinaldi Esteche": {
-    photo: "../img/placeholder-icon.webp",
+    photo: "../img/caina-rinaldi-esteche.jpeg",
     title: "Palestrante",
 
     abstract:
@@ -28,14 +28,16 @@ const speakers = {
       {
         title:
           "Dedos, Palitos e Grafos: Vencendo com Matemática – Um Guia para Dominar Jogos de Estratégia",
-      },
+      },{
+        title: "Hackathon Flutter",
+      }
     ],
     contact: {
       email: "cairinest@hotmail.com",
     },
   },
-  Rafael: {
-    photo: "../img/placeholder-icon.webp",
+  "Rafael": {
+    photo: "../img/rafael-oliveira.jpeg",
     title: "Palestrante",
 
     abstract:
@@ -51,7 +53,7 @@ const speakers = {
     },
   },
   "Mario Araujo Xavier": {
-    photo: "../img/placeholder-icon.webp",
+    photo: "../img/mario-araujo-xavier.jpeg",
     title: "Palestrante",
 
     abstract:
@@ -61,20 +63,17 @@ const speakers = {
         title:
           "KDE Neon & KDE Plasma: venha para o KDE e ganhe experiencia profissional em TI",
       },
-      { title: "Oficina de Python para iniciantes" },
-      { title: "Linkedin Avançado" },
       {
         title:
           "FOSSHUB e o desafio de implantar ferramentas livres no mercado corporativo",
-      },
-      { title: "Dokuwiki e a superimportancia da documentacao na TI" },
+      }
     ],
     contact: {
       email: "marioaxavier7@gmail.com",
     },
   },
   "Matheus de Oliveira Saldanha": {
-    photo: "../img/placeholder-icon.webp",
+    photo: "../img/matheus-saldanha.jpeg",
     title: "Palestrante",
 
     abstract:
@@ -87,7 +86,7 @@ const speakers = {
     },
   },
   "Samuel Cardoso": {
-    photo: "../img/placeholder-icon.webp",
+    photo: "../img/samuel-cardoso.jpeg",
     title: "Palestrante",
 
     abstract:
@@ -103,14 +102,146 @@ const speakers = {
     },
   },
   "Carla Merkle Westphall": {
-    photo: "../img/placeholder-icon.webp",
-    title: "Professora",
+    photo: "../img/carla-westphall.jpeg",
+    title: "Painelista",
 
     abstract:
       "Carla Merkle Westphall é graduada em Ciências da Computação pela UFSC em 1994, obteve o grau de mestre em Ciência da Computação pela UFSC em 1996 e o grau de Doutora em Engenharia Elétrica na área de Segurança em 2000 pela UFSC. Atua na área de Ciência da Computação desde 1987. Participou de vários projetos de pesquisa na área de Segurança de Sistemas. O projeto atual chama-se Segurança nas Redes da Nova Geração, englobando Internet das coisas, computação em nuvem, computação em névoa e redes sem fio. Este projeto está em desenvolvimento no Laboratório de Redes e Gerência (LRG) da UFSC, com a participação de alunos de doutorado e mestrado no Programa de Pós-Graduação em Ciência da Computação (PPGCC) da UFSC, além de alunos de graduação. Tem interesse pelos seguintes temas de pesquisa: segurança em sistemas distribuídos, redes de nova geração, blockchain, segurança na programação e gerenciamento de identidades.",
-    events: [{ title: "Mesa Redonda" }],
+    events: [{ title: "Painel de Segurança" }],
     contact: {
       email: "carla.merkle.westphall@ufsc.br",
+    },
+  },
+  "Jean Everson Martina": {
+    photo: "../img/jean-martina.jpeg",
+    title: "Painelista",
+
+    abstract:
+      "Jean Everson Martina é professor e pesquisador sênior no Laboratório de Segurança em Computação (LabSEC) da Universidade Federal de Santa Catarina (UFSC), e professor visitante na Universidade de Hertfordshire, no Reino Unido. Sua área de especialização abrange protocolos de autenticação para ambientes de telemedicina, formalização de cerimônias de segurança, protocolos de segurança voltados para IoT (Internet das Coisas) e aplicações de blockchain. Jean é amplamente reconhecido por seu trabalho em criptografia, com ênfase na segurança de documentos digitais, infraestrutura de chaves públicas (ICP-Brasil), e criptografia resistente a ataques de computadores quânticos.",
+    events: [{ title: "Painel de Segurança" }],
+    contact: {
+      email: "jean.martina@ufsc.br",
+    },
+  },
+  "Thaís Bardini Idalino": {
+    photo: "../img/thais-bardini.jpeg",
+    title: "Painelista",
+
+    abstract:
+      "Thaís Bardini Idalino é Professora Adjunta de Ciência da Computação na Universidade Federal de Santa Catarina (UFSC), onde atua desde 2021. Possui doutorado em Ciência da Computação pela University of Ottawa, no Canadá, mestrado e graduação em Ciência da Computação pela UFSC. Durante seu doutorado, desenvolveu uma tese sobre tolerância a falhas em aplicações criptográficas utilizando famílias livre de cobertura (cover-free families), uma técnica avançada da combinatória aplicada à segurança da informação.",
+    events: [{ title: "Painel de Segurança" }],
+    contact: {
+      email: "thaisidalino@gmail.com",
+    },
+  },
+  "Mateus Cechetto": {
+    photo: "../img/matheus-cechetto.jpeg",
+    title: "Palestrante",
+
+    abstract:
+      "Formado em Sistemas de Informação na UFSC, trabalho como engenheiro de software desde 2018, participando em projetos na área da educação, hospitalar, engenharia e, atualmente, gamer. Nos últimos anos me aproximei de projetos open source, principalmente na comunidade de Hearthstone, tendo contribuido pra múltiplos projetos e sendo o criador de um projeto com mais de 6000 visitantes e mantenedor de outro com milhões de downloads.",
+    events: [{ title: "Projetos Pessoais: De Hobby a Carreira Internacional" }],
+    contact: {
+      email: "mateuscechetto@gmail.com",
+    },
+  },
+  "Eduardo Perottoni": {
+    photo: "../img/eduardo-perottoni.jpeg",
+    title: "Palestrante",
+
+    abstract:
+      "Estudante de Ciência da Computação na UFSC (7º fase) e bolsista do LabSEC, onde atua no projeto iekuatiara.",
+    events: [{ title: "ADES: Assinador Digital com Certificados de Uso Único do LabSEC" }],
+    contact: {
+      email: "edu.perottoni@gmail.com",
+    },
+  },
+  "Gustavo": {
+    photo: "../img/Gustavo-Kundlatsch.jpeg",
+    title: "Palestrante",
+
+    abstract:
+      "Sou da turma de 2017.1, me formei no começo de 2022. Trabalhei com teoria de agentes no IATE durante 3 anos, desenvolvendo meu TCC na área de percepção. Ainda na graduação trabalhei no Bridge como desenvolvedor da equipe de suporte do eSUS APS, e depois de me formar trabalhei por dois anos e meio em uma legaltech, atuando como backend e mais tarde como techlead da área de desenvolvimento web. Atualmente trabalho com o treinamento de LLMs.",
+    events: [{ title: "Clean code ainda é clean?" }, { title: "Minicurso de LaTeX" }],
+    contact: {
+      email: "gustavo.kundlatsch@gmail.com",
+    },
+  },
+  "Pedro Veiga": {
+    photo: "../img/pedro-veiga.jpeg",
+    title: "Palestrante",
+
+    abstract:
+      "Pedro Veiga é cofundador da MOIC Digital e possui ampla experiência no mercado de criptoativos. Atuou na maior gestora de investimentos do sul do Brasil e em empresas internacionais do setor de cripto. Com envolvimento no mercado web3 desde 2018, hoje atua como Head of Research da MOIC Digital.",
+    events: [{ title: "Oportunidades em Blockchain: Inovação e a Importância da Segurança" }],
+    contact: {
+      email: "pedro@moicdigital.com",
+    },
+  },
+  "Claudio Gerolimetto": {
+    photo: "../img/Claudio-Gerolimetto.jpeg",
+    title: "Palestrante",
+
+    abstract:
+      "Claudio Gerolimetto é desenvolvedor especializado em Solidity e expert em tecnologias relacionadas ao ecossistema Ethereum. Atua na pesquisa e desenvolvimento para empresas focadas em criação e auditoria de contratos inteligentes, contribuindo para o aprimoramento de soluções em blockchain.",
+    events: [{ title: "Oportunidades em Blockchain: Inovação e a Importância da Segurança" }],
+    contact: {
+      email: "pedro@moicdigital.com",
+    },
+  },
+  "Luciano Faustino": {
+    photo: "../img/luciano-faustino.jpeg",
+    title: "Palestrante",
+
+    abstract:
+      "Atua há mais de 18 anos com sistemas Unix-like e redes, e há 7 anos com foco em arquitetura Multi Cloud, infraestrutura como código, automação, integração contínua, monitoramento, segurança e containers. Atualmente, faz parte de um time dedicado à implementação da cultura DevOps na Neoway, maior empresa de Big Data Analytics e Inteligência Artificial da América Latina, capacitando engenheiros(as) de software e otimizando a eficiência operacional da plataforma. Também é ativo na organização de eventos e no compartilhamento de conhecimento em comunidades de DevOps, Cloud e Docker em Florianópolis, já tendo organizado encontros do Docker Meetup Florianópolis.",
+    events: [{ title: "Genesis: Segurança na Cloud by Design usando Infrastructure as Code (IaC)" }],
+    contact: {
+      email: "luciano.faustino@neoway.com.br",
+    },
+  },
+  "Ricardo Goes": {
+    photo: "../img/ricardo-goes.jpeg",
+    title: "Palestrante",
+
+    abstract:
+      "Engenheiro formado pelo IME (Instituto Militar de Engenharia) com MBA em Gestão Estratégica de Tecnologia da Informação pela UFRJ. PMP (Project Management Professional) desde 2008 com mais de 20 anos de experiência em gerenciamento de projetos de alto risco e complexidade.",
+    events: [{ title: "Trabalho Remoto em empresas de tecnologia" }],
+    contact: {
+      email: "rsagoes@gaudium.com.br",
+    },
+  },
+  "Bruno Muniz": {
+    photo: "../img/bruno-muniz.jpeg",
+    title: "Palestrante",
+
+    abstract:
+      "É formado em Ciência da Computação (UECE) e Mestre em Informática (PUC-Rio). Teve seu primeiro contato com tecnologia aos 10 anos de idade e desde então nunca mais parou. Está no espírito e no sangue construir soluções tecnológicas práticas para resolver as dores das pessoas e empresas. Transita bem como líder multiplicador de propósito e cultura, e também em diferentes áreas como Estratégia, Vendas, Marketing, Produto, Programação e Infraestrutura.",
+    events: [{ title: "Trabalho Remoto em empresas de tecnologia" }],
+    contact: {
+      email: "munizbc@gaudium.com.br",
+    },
+  },
+  "Shirlei Chaves": {
+    photo: "../img/shirlei-chaves.jpeg",
+    title: "Palestrante",
+
+    abstract:
+      "Sou doutoranda em Ciência da Computação na Universidade Federal de Santa Catarina (UFSC) e Analista de TI no Instituto Federal de Santa Catarina (IFSC). Meus interesses de pesquisa incluem privacidade e proteção de dados, usabilidade em privacidade e segurança, gerenciamento de identidades digitais e Interação Humano-Computador (IHC).",
+    events: [{ title: "Autenticação de usuários: antigos desafios e novas tecnologias" }],
+    contact: {
+      email: "shirlei@gmail.com",
+    },
+  },
+  "Miliane Fantonelli": {
+    photo: "../img/miliane-fantonelli.jpeg",
+    title: "Palestrante",
+
+    abstract:
+      "Mestre em Teoria e Filosofia do Direito no Programa de Pós-Graduação em Direito na Universidade Federal de Santa Catarina (UFSC). Especialista em Proteção de Dados & Compliance Digital pela Universidade Presbiteriana Mackenzie. Especialista em Saúde Digital pela Universidade Federal de Goiás (UFG). Formada em Direito pela Universidade Federal de Santa Maria (UFSM), com láurea acadêmica. Atuo como Advogada e Data Protection Officer no Laboratório Bridge desde 2019, implementando os preceitos da proteção dos dados em softwares e processos e desenvolvendo pesquisas científicas nas áreas de privacidade, cibersegurança e tecnologia. Professora. Advogada.",
+    events: [{ title: "Proteção de dados no processo de desenvolvimento" }],
+    contact: {
+      email: "miliane@bridge.ufsc.br",
     },
   },
 };
